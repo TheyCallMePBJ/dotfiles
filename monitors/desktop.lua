73dc9284033a683e5c76d92f3dcfb9a069e3198b
TheyCallMePBJ <manomay.gadiyar@gmail.com>
2026-07-24 13:38:07 +0530

    Fixed workspaces in desktop.lua

--- monitors/desktop.lua
+++ monitors/desktop.lua
@@ -11,3 +11,11 @@ hl.monitor({
   scale=1.0,
   transform=1,
 })
+
+for i=1,5 do
+  hl.workspace_rule({workspace=i, monitor="DP-1"})
+end
+
+for i=6,10 do
+  hl.workspace_rule({workspace=i, monitor="DP-2"})
+end
